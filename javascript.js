bb2450d6744188e319f7eee031c030f7e7a3eab6
Maxim <maks16082013@gmail.com>
2019-04-04 21:51:14 +0400

    ggg

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// 1
+var x = 60 * 60 * 3;
+console.log(x);
+// 2
+var num = 1;
+num = ((num + 12) - 14) * 5 / 7 + 1 - 1;
+alert(num);
+// 3
+var a = 10;
+var b = 2;
+var c = 5;
+alert(a + b + c);
+// 4
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7
+result = c + d;
+alert(result);
+// 5
+var name = 'Maxim';
+alert('Привет,' + name + '!');
